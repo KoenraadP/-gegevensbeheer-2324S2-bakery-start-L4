4f24a5d7fd08baf3069be2ed027e02acc8c3fed5
Koenraad Pecceu <koenraad.pecceu@creo.be>
2024-03-07 11:34:04 +0100

    Add code for read all to ProductDal

---
+++ Bakery.Dal/ProductDal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bakery.Entities;
+
+namespace Bakery.Dal
+{
+    public static class ProductDal
+    {
+        // CREATE
+
+        // READ ALL
+        public static List<Product> Read()
+        {
+            using (var db = new BakeryDbContext())
+            {
+                // alle records uit tabel Products halen
+                // en opslaan als List in C#
+                List<Product> lstProducts = db.Products.ToList();
+                return lstProducts;
+            }
+        }
+
+        // UPDATE
+
+        // DELETE
+    }
+}
