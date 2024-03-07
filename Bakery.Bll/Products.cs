7705f146340cc3b77225d4468df503a748659357
Koenraad Pecceu <koenraad.pecceu@creo.be>
2024-03-07 11:38:05 +0100

    Add read all methode to Products in Bll

---
+++ Bakery.Bll/Products.cs
@@ -0,0 +1,21 @@
+﻿using Bakery.Dal;
+using Bakery.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bakery.Bll
+{
+    public static class Products
+    {
+        // READ ALL
+        // eigenlijk wil ik hier gewoon 
+        // de read methode uit ProductDal uitvoeren
+        public static List<Product> Read()
+        {
+            return ProductDal.Read();
+        }
+    }
+}
