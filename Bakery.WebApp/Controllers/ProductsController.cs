e41f788e65b6a681e687a6f7d45c2a011666528c
Koenraad Pecceu <koenraad.pecceu@creo.be>
2024-03-07 11:13:37 +0100

    Add productscontroller

---
+++ Bakery.WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Bakery.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bakery.WebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // READ ALL
+        public ActionResult Index()
+        {
+            List<Product> lstProducts = new List<Product>();
+            return View(lstProducts);
+        }
+    }
+}
