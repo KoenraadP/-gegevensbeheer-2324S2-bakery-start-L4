80da8e86900cda6e7bfe920ed588951f7b612600
Koenraad Pecceu <koenraad.pecceu@creo.be>
2024-03-07 09:58:21 +0100

    Add all seed data

--- Bakery.Dal/Migrations/Configuration.cs
+++ Bakery.Dal/Migrations/Configuration.cs
@@ -28,7 +28,15 @@
             // tabel Categories in databank
             context.Categories.AddOrUpdate(c1, c2);
 
+            // twee test producten maken 
+            Product p1 = new Product("Eclair");
+            Product p2 = new Product("Groot bruin brood",
+                "Brood...dat groot en bruin is", "brownbread.jpg",
+                2.3M, true, c1);
 
+            // producten Eclair en Groot bruin brood
+            // toevoegen aan tabel Products in databank
+            context.Products.AddOrUpdate(p1, p2);
         }
     }
 }
